0dcbfd778c764f17eb2987b75695fb76588af196
DichLe <86064365+DichLe@users.noreply.github.com>
2021-06-20 16:22:04 +0700

    Add project files.

---
+++ 04_D01/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_D01.Controllers
+{
+    public class HomeController : Controller
+    {
+        DB01Entities entity = new DB01Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetMenu()
+        {
+            var menu = entity.LoaiSPs.ToList();
+            return PartialView("PartialMenu", menu);
+        }
+
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+        public ActionResult SPTheoLoai(int maLoai, int? index)
+        {
+            var pageSize = 10 / 2;
+            index = index ?? 1;
+            var dssp = entity.SanPhams.Where(s => s.MaLoai == maLoai).ToList();
+            return View(dssp);
+        }
+
+        public ActionResult ChiTietSP(int id)
+        {
+            var rs = entity.SanPhams.FirstOrDefault(s => s.MaSP == id);
+            ViewBag.thuongHieu = entity.LoaiSPs.FirstOrDefault(l => l.MaLoai == rs.MaLoai).Ten;
+            ViewBag.SPTuongTu = entity.SanPhams.Where(x => x.MaLoai == rs.MaLoai && x.MaSP != rs.MaSP);
+            return View(entity.SanPhams.FirstOrDefault(s => s.MaSP == id));
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 04_D01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_04_D01.MvcApplication" Language="C#" %>
